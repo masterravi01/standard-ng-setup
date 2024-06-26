e860e6c682b274138676fb1736d37bc2bb263cb3
Ravi Parmar <ravi.parmar@simulas.in>
2024-06-26 12:38:53 +0000

    ngx skeleton loader added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgxSkeletonLoaderModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AlertModalComponent } from '../../core/reused/alert-modal/alert-modal.component';
-
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
@@ -18,7 +17,7 @@ const routes: Routes = [
     CommonModule,
     RouterModule,
     RouterModule.forChild(routes),
-    AlertModalComponent,
+    NgxSkeletonLoaderModule,
   ],
 })
 export class DashboardModule {}
