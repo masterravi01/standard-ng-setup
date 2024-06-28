f201ff80e5e224ef6ffafa19a5f4073de9f4a0e3
Ravi Parmar <ravi.parmar@simulas.in>
2024-06-28 05:33:55 +0000

    ag grid rused component added

---
+++ src/app/core/reused/ag-grid-table/ag-grid-table.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community';
+import { AgGridAngular } from 'ag-grid-angular';
+
+@Component({
+  selector: 'app-ag-grid-table',
+  standalone: true,
+  imports: [AgGridAngular],
+  templateUrl: './ag-grid-table.component.html',
+  styleUrl: './ag-grid-table.component.css',
+})
+export class AgGridTableComponent implements OnInit {
+  private gridApi!: GridApi<any>;
+
+  userList: any[] = [];
+  @Input() rowInputData: any[] = [];
+  @Input() colLabels: any[] = [];
+  public rowSelection: 'single' | 'multiple' = 'multiple';
+  defaultColDef = {
+    flex: 1,
+    minWdith: 100,
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('ag grid');
+  }
+  onGridReady(event: GridReadyEvent<any>) {
+    this.gridApi = event.api;
+  }
+
+  onBtExport() {
+    this.gridApi.exportDataAsCsv();
+  }
+}
