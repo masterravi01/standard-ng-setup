250fda122279d2c52f2c1c5a46aa4c1de24dcba5
Ravi Parmar <ravi.parmar@simulas.in>
2024-07-02 18:46:34 +0000

    global ui change

--- src/app/pages/global-loader/global-loader.component.ts
+++ src/app/pages/global-loader/global-loader.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LoaderService } from '../../core/services/loader.service';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-global-loader',
@@ -14,4 +15,12 @@ export class GlobalLoaderComponent {
   loading$ = this.loaderService.loading$;
 
   constructor(private loaderService: LoaderService) {}
+
+  isUserLogin() {
+    if (localStorage.getItem(environment.storageKey)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
